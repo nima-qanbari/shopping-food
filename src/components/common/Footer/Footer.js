172e61aeb0a1c6db155aae8cfdbc6e6eef4f3dc4
nima <nimaqanbari1999@gmail.com>
2022-04-08 07:05:57 -0700

    complete footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+
+//components
+import Logo from "../Logo/Logo";
 
 //styles
-import "./styles.css"
+import "./styles.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <p>
+        We're a team of Professional cooks who are exited about their food,
+        amazing skills and experience and expertiness in cooking.
+      </p>
+      <Logo />
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
