0aee9a3b75865dcad04b19440d8adc939cef2485
nima <nimaqanbari1999@gmail.com>
2022-04-10 03:35:41 -0700

    add my name in footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -3,6 +3,9 @@ import React from "react";
 //components
 import Logo from "../Logo/Logo";
 
+//react-icons
+import {BsSuitHeartFill} from "react-icons/bs"
+
 //styles
 import "./styles.css";
 
@@ -13,6 +16,7 @@ const Footer = () => {
       ما تیمی از آشپزهای حرفه ای هستیم که در مورد غذا، مهارت های شگفت انگیز و تجربه و تخصص خود را در آشپزی در اختیار شما قرار میدهیم .
       </p>
       <Logo />
+      <h5>made by <span><BsSuitHeartFill/></span> nima qanbari</h5>
     </footer>
   );
 };
